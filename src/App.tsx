3bff28953f5d6f66db01a36071ed5f50c1687b7c
Thiago Spart <user000.000resu@gmail.com>
2026-07-20 02:07:20 -0300

    feat: debounce autosave and show save-status indicator

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 import { Scene } from './scene/Scene';
 import { MeasurementPanel } from './ui/MeasurementPanel';
 import { Legend } from './ui/Legend';
 import { RotateButton } from './ui/RotateButton';
 import { ResponsiveLayout } from './ui/ResponsiveLayout';
+import { SaveStatus } from './ui/SaveStatus';
 import { useKeyboardShortcuts } from './hooks/useKeyboardShortcuts';
 import { useSceneStore } from './store/sceneStore';
+import { debounce } from './lib/debounce';
 
 export function App() {
   useKeyboardShortcuts();
@@ -13,20 +15,23 @@ export function App() {
   const saveScene = useSceneStore((s) => s.saveScene);
   const pieces = useSceneStore((s) => s.pieces);
 
+  const debouncedSave = useMemo(() => debounce(saveScene, 600), [saveScene]);
+
   useEffect(() => {
     loadScene();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
-    saveScene();
-  }, [pieces, saveScene]);
+    debouncedSave();
+  }, [pieces, debouncedSave]);
 
   return (
     <ResponsiveLayout
       scene={<Scene />}
       panel={
         <div className="flex flex-col gap-3">
+          <SaveStatus />
           <Legend />
           <MeasurementPanel />
           <RotateButton />
